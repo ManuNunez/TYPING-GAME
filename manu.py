48c7a8053de533c789f94977fbfc2038514a79ee
Manu Nuñez <manu.nunez.2004@gmail.com>
2022-11-17 14:47:49 -0600

    avance de sort

---
+++ manu.py
@@ -0,0 +1,10 @@
+def sort_scores(arg_to_sort):
+    
+    if arg_to_sort == 1:
+        pass
+    elif arg_to_sort == 2:
+        pass
+    elif arg_to_sort == 3:
+        pass
+    else:
+        print("el argumento introducido es invalido")
