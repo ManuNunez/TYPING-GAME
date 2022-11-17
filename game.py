649c8a6b89bd4bdca93256481d7bb6baac1f2eaf
Alejandro <mvegatornero@gmai.com>
2022-11-17 14:35:58 -0600

    Function list

--- game.py
+++ game.py
@@ -67,6 +67,30 @@ def sign_up():
 def log_out(user_logout):
     user = "default"
     menu(user)
+
+#Function list
+listpalabrasf[agua,diamante,luz,calor,intensidad,ropa,cama,jabón,salida,cansancio,lápiz,saludo,carpeta,leche,silla]
+listapalabrasin[variable,agua,intensidad,lenguaje,numero,cama,codigo,binario,computadora,tecla,carpeta,leche,agua,letra,camara,link,metrica,coordenada]
+listapalabrasdif[lenguaje,intensidad,binario,computadora,letra,camara,complejo,variable,diamante,luz,calor,jabon,salida,carpeta,leche,cortafuegos,paraguas,telescopio,universo,gramatica,parangaricutirimicuaro,celular]
+
+def listas(dificultad):
+    list = []
+    if dificultad == 1:
+        v1= random(10-15)
+        for i in range (0,v1):
+            list.append(listpalabrasf[random(0,14)])
+            
+    elif dificultad == 2:
+        v1= random(15-20)
+        for i in range (0,v1):
+             list.append(listapalabrasin[random(0,19)])
+
+    elif == 3:
+        v1=random(20-25)
+        for i in range (0,v1):
+             list.append(listapalabrasdif[random(0,24)])
+    return list
+
 #funtion scores
 
 def scores():
@@ -95,8 +119,8 @@ def comparador(lista_original, lista_usuario):
     total_score = n / len(lista_original)
     return total_score
     
-def game ():
-    pass
+def game (difucultad):
+   function(dificultad) 
 
 
 
